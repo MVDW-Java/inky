2c2ed3c683eda30b2d1c5363cad576d926300e34
MVDW <maxvdwolf@gmail.com>
2023-12-18 14:11:22 +0100

    Update 1-blockall.py

--- experiments/1-blockall.py
+++ experiments/1-blockall.py
@@ -1,22 +1,22 @@
-from icaplib import Server, Response
+from pyicap import Server, BaseICAPRequestHandler
 
-class MyICAPHandler:
-    def __init__(self):
-        pass
+class MyICAPHandler(BaseICAPRequestHandler):
+    def _handle_reqmod(self):
+        # Implement your safety-checking logic here
+        # For simplicity, let's assume we always block the request
+        self.send_response(403)
+        self.send_header('ISTag', '"W3E4R5T6Y7U8I9O0"')
+        self.send_header('Encapsulated', 'req-hdr=0, null-body=8960')
+        self.end_headers()
+        self.send('HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n')
 
-    def reqmod(self, icap):
-        print(icap)
+    def _handle_resmod(self):
+        # Implement response modification logic if needed
+        self.send_response(204)
+        self.send_header('ISTag', '"W3E4R5T6Y7U8I9O0"')
+        self.send_header('Encapsulated', 'res-hdr=0, null-body=8960')
+        self.end_headers()
+        self.send('')  # Empty response body
 
-        icap.set_icap_response(Response.CONTINUE)
-        icap.set_enc_status("HTTP/1.1 403 Forbidden")
-        icap.set_enc_header("Content-Type", "text/html")
-        icap.set_enc_header("Content-Length", "0")
-        icap.send_headers()
-        icap.send_content()
-        icap.done()
-
-    def respmod(self, icap):
-        icap.done()
-
-icap_server = Server(handler=MyICAPHandler(), port=1344)
+icap_server = Server(handler=MyICAPHandler)
 icap_server.run()
