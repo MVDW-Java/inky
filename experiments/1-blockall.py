f82ca06a8e6305627e219bec1dc34fe84db8bc3a
MVDW <maxvdwolf@gmail.com>
2023-12-18 16:54:12 +0100

    Update 1-blockall.py

--- experiments/1-blockall.py
+++ experiments/1-blockall.py
@@ -1,22 +1,38 @@
-from pyicap import Server, BaseICAPRequestHandler
+import pyicap
+from pyicap import BaseICAPRequestHandler, ICAPServer
 
-class MyICAPHandler(BaseICAPRequestHandler):
-    def _handle_reqmod(self):
-        # Implement your safety-checking logic here
-        # For simplicity, let's assume we always block the request
-        self.send_response(403)
-        self.send_header('ISTag', '"W3E4R5T6Y7U8I9O0"')
-        self.send_header('Encapsulated', 'req-hdr=0, null-body=8960')
-        self.end_headers()
-        self.send('HTTP/1.1 403 Forbidden\r\nContent-Length: 0\r\n\r\n')
+class URLBlockerHandler(BaseICAPRequestHandler):
+    def options(self):
+        self.set_icap_response(200)
+        self.set_icap_header('Methods', 'REQMOD')
 
-    def _handle_resmod(self):
-        # Implement response modification logic if needed
-        self.send_response(204)
-        self.send_header('ISTag', '"W3E4R5T6Y7U8I9O0"')
-        self.send_header('Encapsulated', 'res-hdr=0, null-body=8960')
-        self.end_headers()
-        self.send('')  # Empty response body
+    def reqmod(self):
+        # Get the original HTTP request
+        http_request = self.get_enc_req()
 
-icap_server = Server(handler=MyICAPHandler)
-icap_server.run()
+        # Extract the URL from the request
+        url = http_request.headers.get('Host', '') + http_request.uri
+
+        # List of blocked domains
+        blocked_domains = ["example.com"]
+
+        print("request: ", url)
+
+
+        # Check if the requested URL is in the blocked domains list
+        if any(domain in url for domain in blocked_domains):
+            # If the URL is blocked, respond with HTTP 403 Forbidden
+            self.set_icap_response(403)
+            self.send_headers()
+            self.send_response(b'Access to this URL is blocked')
+        else:
+            # If the URL is not blocked, pass the request through
+            self.no_adaptation_required()
+
+if __name__ == "__main__":
+    server = ICAPServer(('127.0.0.1', 1344), URLBlockerHandler)
+    try:
+        print("ICAP server started on icap://127.0.0.1:1344")
+        server.serve_forever()
+    except KeyboardInterrupt:
+        print("ICAP server stopped.")
